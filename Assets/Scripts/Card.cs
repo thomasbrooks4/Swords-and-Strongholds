46c36b958601f4c7fda08f7e1be753eddb3196e1
Thomas Brooks <brooks.thomas67@gmail.com>
2016-10-25 16:28:23 -0700

    Creating Piece and Card Classes
    - Initialized Card class
    - Developing Piece class and functions

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+	public virtual void PlayCard() {
+		
+	}
+}
+
+public class SwordCard :Card {
+	public override void PlayCard() {
+		
+	}
+}
+
+public class StrongholdCard :Card {
+	public override void PlayCard() {
+		
+	}
+}
+
+public class DiplomacyCard :Card {
+	public override void PlayCard() {
+		
+	}
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece : MonoBehaviour {
+	bool isGrey = true;
+	bool isDead = false;
+	bool isSelected = false;
+	bool isStronghold = false;
+
+	Point position;
+
+	public bool IsGrey () {
+		return isGrey;
+	}
+
+	public bool IsDead () {
+		return isDead;
+	}
+
+	public bool IsSelected () {
+		return isSelected;
+	}
+
+	public bool IsStronghold () {
+		return isStronghold;
+	}
+
+	public void Select () {
+		isSelected = true;
+	}
+
+	public void Deselect () {
+		isSelected = false;
+	}
+
+	public Point GetPosition() {
+		return position;
+	}
+
+	public void SetPosition(int x, int y) {
+		position.SetPoint (x, y);
+	}
+
+	public void Death () {
+		isDead = true;
+		position = null;
+	}
+}
+
+public class Point {
+	int x;
+	int y;
+
+	public void SetPoint(int a, int b) {
+		x = a;
+		y = b;
+	}
+}
