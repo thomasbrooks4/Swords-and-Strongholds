9d9bcb61d29d76362b7673fe8c08632125b594c0
thomasbrooks4 <brooks.thomas67@gmail.com>
2016-12-06 15:48:17 -0800

    Added Main Menu
    - Added a Main Menu Scene that transitions to Board scene after hitting
    Play button

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+   public void OnPlay() {
+      SceneManager.LoadScene("Board");
+   }
+
+   public void OnExit() {
+      Application.Quit();
+   }
+}
