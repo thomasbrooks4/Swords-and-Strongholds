9390cbe36214d1f339770ec49316c5ebf8d88620
thomasbrooks4 <brooks.thomas67@gmail.com>
2016-12-08 01:52:22 -0800

    Added Instructions Screen
    - Added empty instructions screen that will be filled with instructions
    images
    - Tweaked menus to be more uniform

---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour {
+   public void OnPlay() {
+      SceneManager.LoadScene("Board");
+   }
+
+   public void OnInstructions() {
+      SceneManager.LoadScene("Instructions");
+   }
+
+   public void OnExit() {
+      Application.Quit();
+   }
+
+   public void OnMainMenu() {
+      SceneManager.LoadScene("Main Menu");
+   }
+}
